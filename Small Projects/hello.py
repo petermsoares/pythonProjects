b0458302286675253916b61e073a08511954f24f
Peter <petersoares52@gmail.com>
2023-09-05 01:58:34 -0400

    Notes added for Ch 1.

--- Small Projects/hello.py
+++ Small Projects/hello.py
@@ -1,12 +1,3 @@
-judgeInput = int(input())
-successfulProblems = 0
-rightAnswers = ["1 1 0", "1 0 1", "0 1 1", "1 1 1"]
-
-for i in range(0, judgeInput, 1):
-    problemSolve = input()
-    if problemSolve in rightAnswers:
-        successfulProblems = successfulProblems + 1
-    else:
-        continue
-
-print(successfulProblems)
+bacon = 20
+bacon + 1
+print(bacon)
